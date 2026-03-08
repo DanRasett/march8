e5b2b9111e269f141a3ebb8a43a6c29cf3decd13
DanRasett <81423509+DanRasett@users.noreply.github.com>
2026-03-08 15:20:31 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,14 @@
-import {useState} from "react"
-import BackgroundHearts from "./components/BackgroundHearts"
-import RosePetals from "./components/RosePetals"
-import Envelope3D from "./components/Envelope3D"
-import CardGallery from "./components/CardGallery"
-import FinalScreen from "./components/FinalScreen"
-import "./styles/global.css"
+import { useState } from "react";
+import CardGallery from "./components/CardGallery";
+import FinalScreen from "./components/FinalScreen";
 
-export default function App(){
+export default function App() {
+  const [stage, setStage] = useState(1); // сразу открываем карточки для теста
 
-const[stage,setStage]=useState(0)
-
-return(
-
-<div className="app">
-
-<BackgroundHearts/>
-<RosePetals/>
-
-{stage===0 &&
-
-<Envelope3D
-onOpen={()=>setStage(1)}
-/>
-
-}
-
-{stage===1 &&
-
-<CardGallery
-onFinish={()=>setStage(2)}
-/>
-
-}
-
-{stage===2 &&
-
-<FinalScreen/>
-
-}
-
-</div>
-
-)
-
+  return (
+    <div className="app">
+      {stage === 1 && <CardGallery onFinish={() => setStage(2)} />}
+      {stage === 2 && <FinalScreen />}
+    </div>
+  );
 }
